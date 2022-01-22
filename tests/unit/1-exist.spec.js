a3bc641b0ed97347e9ff2eed85f9d714655530d4
OGurel24 <onur.gurel@useinsider.com>
2022-01-22 23:02:01 +0300

    Finalize

--- tests/unit/1-exist.spec.js
+++ tests/unit/1-exist.spec.js
@@ -3,48 +3,44 @@ import App from "../../src/App";
 import store from "../../src/store";
 import Vuex from 'vuex'
 
-
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
+function beforeEach() {
+    const wrapper = mount(App, {localVue, store})
+    const Counter = wrapper.findComponent({name: 'Counter'})
+    const counter = Counter.find('.counter')
+    const decreaseButton = Counter.findAll('.decrease')
+    const increaseButton = Counter.findAll('.increase')
+    store.replaceState({count: 0})
+
+    return {wrapper, Counter, counter, decreaseButton, increaseButton};
+}
+
+const {wrapper, Counter, counter, decreaseButton, increaseButton} = beforeEach()
 
 describe('1- Counter component exist', function () {
     it('Existency check for counter component', function () {
-        const wrapper = mount(App, {localVue, store})
-        const Counter = wrapper.findComponent({name: 'Counter'}) // => finds Bar by `name`
         expect(Counter.exists()).toBe(true)
     })
 })
 
 describe('2- Counter component decrease button existency', function () {
     it('Existency check for counter component decrease button', function () {
-        const wrapper = mount(App, {localVue, store})
-        const Counter = wrapper.findComponent({name: 'Counter'}) // => finds Bar by `name`
-        const decreaseButton = Counter.findAll('.decrease')
         expect(decreaseButton.exists()).toBe(true)
     })
 })
 
 describe('3- Counter component increase button existency', function () {
     it('Existency check for counter component increase button', function () {
-        const wrapper = mount(App, {localVue, store})
-        const Counter = wrapper.findComponent({name: 'Counter'}) // => finds Bar by `name`
-        const decreaseButton = Counter.findAll('.increase')
         expect(decreaseButton.exists()).toBe(true)
     })
 })
 
 describe('4- Counter component decrease button functionality', function () {
     it('Decrease button functionality check for counter component', async function () {
-        const wrapper = mount(App, {localVue, store})
-        const Counter = wrapper.findComponent({name: 'Counter'})
-        const counter = Counter.find('.counter')
         expect(counter.text()).toBe('0k')
-
-
-        const decreaseButton = Counter.findAll('.decrease')
         await decreaseButton.trigger('click')
-
         expect(counter.text()).toBe('-1k')
     })
 })
@@ -52,15 +48,7 @@ describe('4- Counter component decrease button functionality', function () {
 describe('5- Counter component increase button functionality', function () {
     it('Decrease button functionality check for counter component', async function () {
         store.replaceState({count: 0})
-        const wrapper = mount(App, {localVue, store})
-        const Counter = wrapper.findComponent({name: 'Counter'})
-        const counter = Counter.find('.counter')
-        expect(counter.text()).toBe('0k')
-
-
-        const increaseButton = Counter.findAll('.increase')
         await increaseButton.trigger('click')
-
         expect(counter.text()).toBe('1k')
     })
 })
@@ -68,18 +56,9 @@ describe('5- Counter component increase button functionality', function () {
 describe('6- 2 Increase 1 decrease sequentially', function () {
     it('Use increase and decrease sequentially', async function () {
         store.replaceState({count: 0})
-        const wrapper = mount(App, {localVue, store})
-        const Counter = wrapper.findComponent({name: 'Counter'})
-        const counter = Counter.find('.counter')
-
-        const decreaseButton = Counter.findAll('.decrease')
-        const increaseButton = Counter.findAll('.increase')
-
         await increaseButton.trigger('click')
         await increaseButton.trigger('click')
         await decreaseButton.trigger('click')
-
-
         expect(counter.text()).toBe('1k')
     })
 })
@@ -87,14 +66,8 @@ describe('6- 2 Increase 1 decrease sequentially', function () {
 describe('7- Count text visual test', function () {
     it('Check classes according to counts', async function () {
         store.replaceState({count: 0})
-        const wrapper = mount(App, {localVue, store})
-        const Counter = wrapper.findComponent({name: 'Counter'})
-        const decreaseButton = Counter.findAll('.decrease')
-        const increaseButton = Counter.findAll('.increase')
-
 
         const notificationArea = wrapper.find('.notificationArea')
-        const classes = notificationArea.classes()
         expect(notificationArea.classes()).toContain('safe')
 
         for (let i = 0; i < 4; i++) {
@@ -111,6 +84,5 @@ describe('7- Count text visual test', function () {
             await increaseButton.trigger('click')
             expect(notificationArea.classes()).toContain('danger')
         }
-
     })
 })
